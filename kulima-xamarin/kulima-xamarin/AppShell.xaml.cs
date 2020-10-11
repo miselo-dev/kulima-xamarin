f28bb69040e6f65ec957f03aad8385a5e8940b58
Miselo Kangwa <miselo.dev@gmail.com>
2020-10-11 01:08:59 +0100

    Add project files.

---
+++ kulima-xamarin/kulima-xamarin/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using kulima_xamarin.ViewModels;
+using kulima_xamarin.Views;
+using Xamarin.Forms;
+
+namespace kulima_xamarin
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ kulima-xamarin/kulima-xamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using kulima_xamarin.ViewModels;
+
+namespace kulima_xamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
